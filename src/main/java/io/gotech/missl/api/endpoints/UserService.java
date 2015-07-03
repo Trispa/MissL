5a5775442db13842b5ca139b1371bb3f0fd4740a
Aristotte Diasonama <aristotediasonama@yahoo.fr>
2015-07-02 23:37:05 -0400

    Updated pom.xml to include Guice stuff

--- src/main/java/io/gotech/missl/api/endpoints/UserService.java
+++ src/main/java/io/gotech/missl/api/endpoints/UserService.java
@@ -1,13 +1,13 @@
 package io.gotech.missl.api.endpoints;
 
 import io.gotech.missl.api.requests.CreateUserRequest;
-import io.gotech.missl.api.responses.UserInfo;
+import io.gotech.missl.domain.users.User;
 
 import com.google.api.server.spi.config.Api;
 
 @Api(name = "missL", version = "v1")
 public class UserService {
-    public UserInfo create(CreateUserRequest request) {
+    public User create(CreateUserRequest request) {
 	throw new RuntimeException("Not yet Implemented");
     }
 }
